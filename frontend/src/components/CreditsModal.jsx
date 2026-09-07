8be71c1c38adcacba7e6e912b7962a5d10849e1a
Rudrapal Singh Shekhawat <rudrapal@gmial.com>
2026-09-08 01:01:57 +0530

    Update title to Lead Engineer

--- frontend/src/components/CreditsModal.jsx
+++ frontend/src/components/CreditsModal.jsx
@@ -26,7 +26,7 @@ export default function CreditsModal({ onClose }) {
       name: 'Rudrapal Singh Shekhawat',
       year: '2nd Year',
       roles: [
-        { title: 'Website Developer', icon: <Code2 size={14} /> },
+        { title: 'Lead Engineer', icon: <Code2 size={14} /> },
         { title: '1st Year Content', icon: <BookOpen size={14} /> }
       ],
       linkedin: 'https://www.linkedin.com/in/rudrapal-singh-shekhawat-5a57a2377'
