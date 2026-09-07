08a49e8b34cf145aa1d53053f650faf777302314
Rudrapal Singh Shekhawat <rudrapal@gmial.com>
2026-09-08 00:58:51 +0530

    Add Website Developer role for Rudrapal in Credits

--- frontend/src/components/CreditsModal.jsx
+++ frontend/src/components/CreditsModal.jsx
@@ -23,6 +23,7 @@ export default function CreditsModal({ onClose }) {
     {
       name: 'Rudrapal Singh Shekhawat',
       roles: [
+        { title: 'Website Developer', icon: <Code2 size={14} /> },
         { title: '1st Year Content', icon: <BookOpen size={14} /> }
       ],
       linkedin: 'https://www.linkedin.com/in/rudrapal-singh-shekhawat-5a57a2377'
