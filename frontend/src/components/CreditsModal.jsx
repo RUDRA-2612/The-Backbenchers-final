e1deec6a5dcc0f6e881f999d41e1fabe8f94a2fc
Rudrapal Singh Shekhawat <shaansingh101206@gmail.com>
2026-09-19 20:49:06 +0530

    Update Rudra's photo to Rudra_6.png in credits

--- frontend/src/components/CreditsModal.jsx
+++ frontend/src/components/CreditsModal.jsx
@@ -7,7 +7,7 @@ export default function CreditsModal({ onClose }) {
     {
       name: 'Rudrapal Singh Shekhawat',
       year: '2nd Year',
-      image: '/rudra_4.png',
+      image: '/Rudra_6.png',
       nameClass: 'name-rudra',
       roles: [
         { title: 'Web Developer', icon: <Code2 size={14} /> },
